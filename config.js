39dde75d57a47a18d2275c2f5065b0238b72c490
Ender Studios <76491740+sairamkishan300@users.noreply.github.com>
2025-12-05 13:43:05 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    backgroundImage: 'pg.jpg',
-    videoLink: 'https://drive.google.com/drive/folders/1QEAzBGJxXWD827dRPlSNYtPvYHe-G4Ah?usp=sharing',
-    audioPath: 'Pookal pokum jushh.m4a',
-    girlfriendName: 'Jushh'
-}; 
